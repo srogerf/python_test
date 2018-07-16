938962c5b0929163f4028d3be658a7f0ee3b26a9
roger <srogerf@gmail.com>
2018-07-15 18:46:52 -0700

    python_test-1 test issue

---
+++ types/test_strings.py
@@ -0,0 +1,6 @@
+test = 'a string'
+print(len(test))
+print(test[5])
+print(test[-1])
+print(test[2:5])
+print(test[:-2])
---
+++ types/test_types.py
@@ -0,0 +1,8 @@
+
+import math
+print(math.pi)
+
+import random
+print(random.choice([1,5,7,9]))
+
+
